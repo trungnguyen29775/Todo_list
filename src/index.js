107f937ec6aa24a4457a7f367f80a3cc5eaf765d
trungnguyen29775e <you@example.com>
2022-04-26 14:03:56 +0700

    26/04/2022 Trung: Register and Login complete!

--- src/index.js
+++ src/index.js
@@ -4,13 +4,67 @@ const morgan = require('morgan');
 const handlebars= require('express-handlebars');
 const app = express();
 const port = 3000;
-const todoList=[];
-const users =[];
-
+const { Sequelize, DataTypes,Model  } = require('sequelize');
+//variable for login and register
+const bcrypt = require('bcrypt');
+//session
+var session = require('express-session');
+const { userInfo } = require('os');
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
 // app.use(morgan('combined'))
 app.use(express.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname,'public')))
 
+const sequelize = new Sequelize('todo_database', 'root', 'trungntmt2808', {
+  host: 'localhost',
+  dialect: 'mysql'
+})
+
+class User extends Model {}
+
+User.init({
+ 
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    primaryKey:true,
+    allowNull:false
+  },
+  password:
+  {
+    type: DataTypes.STRING,
+    allowNull:false
+  }
+}, {
+  // Other model options go here
+  sequelize, // We need to pass the connection instance
+  modelName: 'User',
+  tableName:"user-info"
+});
+
+async function CheckConnect()
+{
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+async function CreateDatase()
+{
+  await User.sync({force:true});
+  console.log("The table for the User model was just created!");
+}
+
+
+
+CheckConnect();
+// CreateDatase();
 
 app.engine('hbs', handlebars({
   extname: '.hbs'
@@ -41,43 +95,52 @@ app.get('/register',(req,res)=>
 
 app.post('/register',(req,res)=>
 {
-  try{
-    users.push
-    (
+ try{
+    async function CreateUser()
+    {
+      const hashedPassword = await bcrypt.hash(req.body.password,10);
+      return User.create(
       {
-      id: Date.now().toString,
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
       }
-    )
+      
+    );
+    }
+    CreateUser();
     res.redirect('/login')
-  }
-  catch
-  {
-    res.redirect('/register')
-  }
-  console.log(users)
+ }
+ catch
+ {
+  res.redirect('/login')
+ }
 })
 
 app.post('/login',(req,res)=>
 {
-  try
-  {
-    if(req.body.email=="admin@" && req.body.password=="admin")
-      res.redirect('/home')
-    else  
+  try{
+    async function CheckLogin()
     {
-      res.redirect('/login');
+      const identify = await User.findOne({ where: { email:req.body.email } });
+      if (identify === null) {
+        res.redirect('/login')
+      } else {
+        if (bcrypt.compare(req.body.password,identify.password))
+          res.redirect('/home')
+      }
     }
+    CheckLogin();
   }
-  catch
-  {
+  catch{
     res.redirect('/login')
   }
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+
 })
 
+const test = async ()=>{ return await User.findOne({ where: { title: 'admin@' } })};
+console.log(test);
